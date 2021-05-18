e8c2d373e062ec11740a26c1334ba495ab5dc2d0
Your Name <you@example.com>
2021-05-18 17:15:09 +1000

    13123123

--- main/mainApp/views.py
+++ main/mainApp/views.py
@@ -2,49 +2,63 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-
 from .forms import DeviceForm
-
 from .models import *
 
 
 class DeviceCreate(View):
+
     def get(self, request):
         form = DeviceForm
-        return render(request, 'device_create.html', context={'form':form})
+        return render(request, 'device_create.html', context={'form': form})
 
-    def device(self, request):
+    def post(self, request):
         bound_form = DeviceForm(request.POST)
         if bound_form.is_valid():
             new_device = bound_form.save()
             return redirect(new_device)
-        return render(request, 'device_create.html', context={'form':bound_form})
+        return render(request, 'device_create.html', context={'form': bound_form})
 
 
-def device_list(request, category_slug = None):
+def device_list(request, category_slug=None):
 
     category = None
     categories = Category.objects.all()
     devices = Device.objects.all()
 
-    paginator = Paginator(devices, 1)  # Show 5 contacts per page.
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    # coordinate filter data
+    s1x = request.GET.get('s1x', 0)
+    s1y = request.GET.get('s1y', 0)
+    s2x = request.GET.get('s2x', 0)
+    s2y = request.GET.get('s2y', 0)
 
+    # radius filter data
+    r1 = request.GET.get('r1', 0)
+    r2 = request.GET.get('r2', 0)
+
+    # category filter
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        devices = Device.objects.filter(category=category)
+        devices = devices.filter(category=category)
 
-        paginator = Paginator(devices, 1)  # Show 5 contacts per page.
-        page_number = request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
+    # coordinate filter
+    if s1x != '' and s2x != '' and s1y != '' and s2y != '' and s1y != 0 and s2y != 0:
+        devices = devices.filter(latitude__range=(s1x, s2x), longitude__range=(s1y, s2y))
+
+    # radius filter
+    if r2 != 0 and r1 != '' and r2 != '':
+        devices = devices.filter(radius__range=(r1, r2))
+
+    paginator = Paginator(devices, 2)  # Show n contacts per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     return render(request,
                   'device_list.html',
                   {'category': category,
                    'categories': categories,
                    'devices': devices,
-                   'page_obj':page_obj})
+                   'page_obj': page_obj})
 
 
 def device_item(request, device_slug):
